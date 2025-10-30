13e38ac0c637de3959f62e0ebb1ff3a13922727e
Dilpreet Kaur <dilpreet.kaur@hurix.com>
2025-10-30 13:29:08 +0530

    code for demo simulation

---
+++ app.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+def get_user_data(username):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    # Intentional SQL injection vulnerability
+    query = f"SELECT * FROM users WHERE name = '{username}'"
+    cursor.execute(query)
+    return cursor.fetchall()
---
+++ config.py
@@ -0,0 +1,1 @@
+API_KEY = "ghp_FAKE1234567890abcdefghijklmnop"

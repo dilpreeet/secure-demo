44b9d8bc958799e1af81ef1677196c948272d9e4
Dilpreet Kaur <dilpreet.kaur@hurix.com>
2025-11-10 19:56:36 +0530

    Code Updates

--- config.py
+++
@@ -1,7 +0,0 @@
-# demo key
-
-API_KEY = "ghp_FAKE1234567890abcdefghijklmnop"
-
-# config.py  (demo only — fake key)
-AWS_ACCESS_KEY_ID = "AKIAEXAMPLEKEY12345"
-AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
